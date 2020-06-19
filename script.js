9017551792bd79f184e6f54da80cf39db8fe46dd
smathersredmond <58798029+smathersredmond@users.noreply.github.com>
2020-06-19 12:21:13 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-function checkDriverAge(){
-	var age = prompt("What is your age?");
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+console.log(body)
 
+color1.addEventListener("input", function(){
+	body.style.background =
+	"linear-gradient( to right, "
+	+ color1.value
+	+ ", "
+	+ color2.value
+	+")";
+	
+})
 
-if (Number(age) < 18) {
-	alert("Sorry, you are too yound to drive this car. Powering off");
-} else if (Number(age) > 18) {
-	alert("Powering On. Enjoy the ride!");
-} else if (Number(age) === 18) {
-	alert("Congratulations on your first year of driving. Enjoy the ride!");
-}
-}
+color2.addEventListener("input", function(){
+	body.style.background =
+	"linear-gradient( to right, "
+	+ color1.value
+	+ ", "
+	+ color2.value
+	+")";
+})
